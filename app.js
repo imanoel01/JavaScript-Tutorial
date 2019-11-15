4080e5767d3ee0460d8b48936e9f24421d39ca18
imanoel <emalieze2@gmail.com>
2019-11-15 12:17:20 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+class Developer {
+    constructor(firstname,lastname){
+        this.firstname=firstname;
+        this.lastname=lastname;
+    }
+
+}
+
+let bio=new Developer("jame", "Montgomeri");
+console.log(bio.firstname+" " + bio.lastname);
+// added a COMMENT
